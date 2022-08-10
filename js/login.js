fd184bace2585dd793316dd184ba6e486f345f10
jnogues93 <nogues93@hotmail.com>
2022-08-10 00:37:02 -0300

    funcionalidades para login

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// Control para no dejar vacio los Input y formato de Email al Presionar Login
+document.getElementById("button_login").onclick = function () {
+email = document.getElementById("email").value;
+passwd = document.getElementById("password").value;
+formatoemail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+valido = document.getElementById('emailOK');
+
+ if( email == "" || passwd == "" ) {
+  alert('Usuario y/o Contraseña incorecto o vacio!');
+ }if (formatoemail.test(email)) {
+    location.href = "../index.html";
+ }else {
+   valido.innerText = "Formato de email Incorrecto";
+ }
+}
+
