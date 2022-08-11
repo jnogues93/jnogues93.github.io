d8dfd4ef0197900a07d77c7160ad379e59cf3c8b
jnogues93 <jnogues93@gmail.com>
2022-08-11 17:00:20 -0300

    cambio login

--- js/login.js
+++ js/login.js
@@ -7,8 +7,8 @@ valido = document.getElementById('emailOK');
 
 
  if( email == "" || passwd == "" ) {
-  alert('Usuario y/o Contraseña Incorecto o Vacio!');
-  window.location.reload()
+  alert('Usuario y/o Contraseña Incorrecto o Vacio!');
+  //window.location.reload()
  }if (formatoemail.test(email)) {
     location.href = "../main.html";
  }else {
@@ -18,7 +18,6 @@ valido = document.getElementById('emailOK');
 
 //Boton para login de Google
 function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
@@ -27,7 +26,6 @@ function onSignIn(googleUser) {
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail());
 
-  // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
 }
