3f505e646d989a80c394e7f9ca168d3d4c65312d
jnogues93 <jnogues93@gmail.com>
2022-08-15 17:58:27 -0300

    Cambios de products y login

--- js/init.js
+++ js/init.js
@@ -39,3 +39,25 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+function JSONData(url){
+  let result = {}; 
+  return fetch(url) 
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response) {
+        result.status = 'ok';
+        result.data = response; 
+        return result;
+  })
+  .catch(function(error) {
+      result.status = 'error';
+      result.data = error; 
+      return result;
+  });
+}
