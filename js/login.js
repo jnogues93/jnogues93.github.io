e6d3f46227bc7d00cf17d7e6212a8798e09c24d4
jnogues93 <nogues93@hotmail.com>
2022-08-21 15:35:50 -0300

    Login google funciona

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+//#########################################################//
+//Login Normal
 // Control para no dejar vacio los Input y formato de Email al Presionar Login
 document.getElementById("button_login").onclick = function () {
 email = document.getElementById("email").value;
@@ -17,16 +19,64 @@ validopasswd = document.getElementById('passwOK');
   location.href = "../main.html";
  }
 }
+//#########################################################//
 
-//Boton para login de Google
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
 
-  //var id_token = googleUser.getAuthResponse().id_token;
-  //console.log("ID Token: " + id_token);
+//#########################################################//
+//Login de Google
+
+function handleCredentialResponse(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
+  location.href = "main.html"
+  
+  //#########################################################//
+  // Funciones para ver los datos del usuario Gmail en HTML
+  //img.src = responsePayload.picture;
+  //getName.innerHTML = responsePayload.name;
+  //id.innerHTML = responsePayload.sub;
+  //email.innerHTML = responsePayload.email;
+  //container.style.display = 'inline-block';
+  //googleButton.style.display = 'none'
+  //#########################################################//
+}
+
+//#########################################################//
+// Funciones para cargar el boton y la conexion con mi API de Google
+window.onload = function () {
+  google.accounts.id.initialize({
+      client_id: "234182601925-p0np49m5bmjgt3fhnrsrf2rcs63c5sdc.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+      auto_select: true,
+      auto: true
+  });
+  google.accounts.id.renderButton(
+      document.getElementById("google-button"),
+      { theme: "filled_blue", size: "medium", width: '200' }
+  );
+  //#########################################################//
+  // Funcion para cargar cuentas logueadas en ventana flotante
+  //google.accounts.id.prompt();
+  //#########################################################// 
+}
+//#########################################################//
+
+
+//#########################################################//
+// Funcion para desifrar la respuesta del JSON
+function decodeJwtResponse(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+}
+//#########################################################//
+
+//#########################################################//
+//Funcion para cerrar sesion, la requiere el boton de login
+function signOut() {
+  google.accounts.id.disableAutoSelect();
+  location.reload();
+//#########################################################//  
 }
